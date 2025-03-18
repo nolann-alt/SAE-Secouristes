f7b9cc5a0ced2d9c909da07125d87bdad5a3149f
nolann-alt <lescopnolann@gmail.com>
2025-03-18 11:15:50 +0100

    Update

--- src/Secouriste.java
+++ src/Secouriste.java
@@ -1,5 +1,23 @@
 public class Secouriste {
-    public Secouriste {
-
+    private int id;
+    private String nom;
+    private String prenom;
+    private int dateNaissance;
+    private String email;
+    private String tel;
+    private String adressse;
+    
+    public Secouriste(int id, String nom, String prenom, int dateNaissance, String email, String tel, String adresse) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.email = email;
+        this.tel = tel;
+        this.adressse = adresse;
     }
+
+    public void get
+
+
 }
