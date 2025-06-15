7cb14e3e83ff4e04b5d09880770c9ed15833f633
nolann-alt <lescopnolann@gmail.com>
2025-06-15 15:24:35 +0200

    Update SAE-Secouriste (vue + contrôleur) rajout d'une page TableauDeBordAdmin.fxml

---
+++ src/controller/TableauDeBordAdminController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class TableauDeBordAdminController {
+    public void handleProfilClick(MouseEvent mouseEvent) {
+    }
+
+    public void handleCalendrierSecouriste(MouseEvent mouseEvent) {
+    }
+
+    public void handleAlertes(MouseEvent mouseEvent) {
+    }
+}
