152ca51102c7842e0583e13a717767276c2f7eec
Arthur <aj29dz@gmail.com>
2025-06-16 08:18:23 +0200

    Réalisation de DispoMngt

--- src/metier/service/DispoMngt.java
+++ src/metier/service/DispoMngt.java
@@ -1,14 +1,30 @@
 package metier.service;
 
 import metier.persistence.DPS;
+import metier.persistence.Disponibilite;
+
+import java.util.LinkedHashSet;
 
 /**
  *
  */
 public class DispoMngt {
 
-    public void nouvDispo(){}
+    private LinkedHashSet<Disponibilite> listeDispo;
 
-    public void enleverDispo(){}
+    public DispoMngt(){
+
+        listeDispo = new LinkedHashSet<>();
+    }
+
+    public LinkedHashSet<Disponibilite> nouvDispo(Disponibilite dispo){
+        listeDispo.add(dispo);
+        return listeDispo;
+    }
+
+    public LinkedHashSet<Disponibilite> enleverDispo(Disponibilite dispo){
+        listeDispo.remove(dispo);
+        return listeDispo;
+    }
 
 }
